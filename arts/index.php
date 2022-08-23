d63b9233d4298e596826c189c9f4140855c42c12
clem0802 <hhcc0802@gmail.com>
2022-08-23 23:34:01 +0200

    --

--- arts/index.php
+++ arts/index.php
@@ -3,13 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/index.css">
         <title>Blog-articles (TP)</title>
         <link rel="shortcut icon" href="https://images.unsplash.com/photo-1471970471555-19d4b113e9ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjZ8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60">
     </head>
 
     <body>
         <!-- (myPATH) http://localhost/y.doranco-7-OOP/arts/index.php -->
-        <h1>Blog-articless (TP) :</h1>
+        <h1>Blog-articles (TP) :</h1>
         <?php 
             include_once './views/artForm.php';
         ?>
